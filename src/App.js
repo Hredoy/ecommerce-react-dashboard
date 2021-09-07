36c55e6841597e4113855976b5a14126858d259b
MD. Abdur Rahim <nhipphredoy@gmail.com>
2021-09-08 02:20:24 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from "react-bootstrap";
 import Header from './Header';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <h1>Hello World</h1>
-        <button>Simple Button</button>
-        <Button>Bootstrap Button</Button>
+        <h1>E-Comm Project</h1>
+        <Route path="/login">
+            <Login />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
+        <Route path="/add">
+            <AddProduct />
+        </Route>
+        <Route path="/update">
+            <UpdateProduct />
+        </Route>
       </BrowserRouter>
     </div>
   );
