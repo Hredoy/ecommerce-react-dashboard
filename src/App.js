6fab99896dac9ce88800d1300bfb77e2b1ec415d
MD. Abdur Rahim <nhipphredoy@gmail.com>
2021-09-08 02:06:37 +0600

    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from "react-bootstrap";
+import Header from './Header';
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello World</h1>
       <button>Simple Button</button>
       <Button>Bootstrap Button</Button>
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,20 @@
+import {Navbar,Nav,Container,NavDropdown} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 function Header()
 {
     return (
         <div>
-            <h1>Update Product Page</h1>
-            
+            <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
+    <Nav className="me-auto">
+        <Link to="/add">Add Products</Link>
+        <Link to="/update">Update Products</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+    </Nav>
+    </Container>
+  </Navbar>
         </div>
     );
 }
